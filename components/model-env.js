af80d03c94ee344b15d1750e3354e19b1494a07b
Sebastian Wloch <sebastian@creative-area.com>
2022-04-09 19:09:40 +0200

    Add files via upload

--- components/model-env.js
+++ components/model-env.js
@@ -1,33 +1,35 @@
-AFRAME.registerComponent("model-env", {
-  schema: {
-    env: {type: 'map'},
-    envPath: {type: 'string', default: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Goat_Peak%2C_Cascades.jpg/1920px-Goat_Peak%2C_Cascades.jpg'},
-  },
-  
-  init: function() {
-    console.log(this.data.imagePath);
-    
-    if(this.data.env) this.data.envPath = this.data.env?.getAttribute("src");
-    var targetCube = new THREE.WebGLRenderTargetCube(512, 512);
-    var renderer = this.el.sceneEl.renderer;
-    
-    this.el.addEventListener("model-loaded", e => {
-      console.log(e);
-      let mesh = this.el.getObject3D("mesh");
-        var texture = new THREE.TextureLoader().load(
-          this.data.envPath,
-          function() {
-            var cubeTex = targetCube.fromEquirectangularTexture(renderer, texture);
-            mesh.traverse(function(el) {
-              if (el.material) {
-                console.log(el.material);
-                el.material.envMap = cubeTex.texture;
-                el.material.envMapIntensity = 3;
-                el.material.needsUpdate = true;
-              }
-            });
-          }
-        );
-    });
-  }
-});
+/* This component adds an environment map to a imported model. 
+    You can either link a existing image or use a path to an image. 
+    Will use a default image if none is provided. */
+
+    AFRAME.registerComponent("model-env", {
+        schema: {
+          env: {type: 'map'},
+          envPath: {type: 'string', default: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Goat_Peak%2C_Cascades.jpg/1920px-Goat_Peak%2C_Cascades.jpg'},
+        },
+        
+        init: function() {
+          
+          if(this.data.env) this.data.envPath = this.data.env?.getAttribute("src");
+          var targetCube = new THREE.WebGLRenderTargetCube(512, 512);
+          var renderer = this.el.sceneEl.renderer;
+          
+          this.el.addEventListener("model-loaded", e => {
+            let mesh = this.el.getObject3D("mesh");
+              var texture = new THREE.TextureLoader().load(
+                this.data.envPath,
+                function() {
+                  var cubeTex = targetCube.fromEquirectangularTexture(renderer, texture);
+                  mesh.traverse(function(el) {
+                    if (el.material) {
+                      console.log(el.material);
+                      el.material.envMap = cubeTex.texture;
+                      el.material.envMapIntensity = 3;
+                      el.material.needsUpdate = true;
+                    }
+                  });
+                }
+              );
+          });
+        }
+      });
